0f6b6122da449be945139c2a37d79414f40a6a61
bai tao <yixiang374@gmail.com>
2025-01-19 16:27:35 +0800

    upd: 更新配置

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/my-vue3-project/', // 确保此处与你的 GitHub Pages URL 相匹配
   plugins: [
     uni(),
   ],
